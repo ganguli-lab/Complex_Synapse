b2fc155c253eca2d2dedb3baa2454579d9c467be
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2013-10-11 16:25:52 -0700

    mean and stderr of err vs #data for synapse fittng

--- Code/Identification/HowMuchDataErr.m
+++ Code/Identification/HowMuchDataErr.m
@@ -1,52 +1,60 @@
 function [ err_data ] = HowMuchDataErr( truemodel,varargin )
 %err_data=HowMuchDataErr(truemodel) How much data do we need to identify
 %SynapseIdModel truemodel?
-%   err_data = struct(num_data,KLdiv,L2err)
+%   err_data = struct(num_data,KLdiv_mean,KLdiv_err,L2_mean,L2_err)
 
 DataPerChunk=200;
-TestDataPerChunk=50;
 fp=0.5;
 Display=true;
 StartLen=1;
 MaxLen=512;
+NumReps=10;
+SuccessFrac=0.5;
+varargin=assignApplicable(varargin);
+TestDataPerChunk=DataPerChunk;
 varargin=assignApplicable(varargin);
 
 
 
 err_data = struct('num_data',zeros(1,ceil(log2(MaxLen/StartLen))));
-err_data.KLdiv=NaN(size(err_data.num_data));
-err_data.L2err=NaN(size(err_data.num_data));
+err_data.KLdiv_mean=NaN(size(err_data.num_data));
+err_data.KLdiv_err=err_data.KLdiv_mean;
+err_data.L2_mean=err_data.KLdiv_mean;
+err_data.L2_err=err_data.KLdiv_mean;
 
-StartLen=floor(StartLen/2);
-i=0;
-fitsim=truemodel.Simulate(fp,rand(2,DataPerChunk,StartLen));
-testsim=truemodel.Simulate(fp,rand(2,TestDataPerChunk,StartLen));
+StartLen=StartLen/2;
 
-while length(fitsim)<MaxLen
-    DoubleData;
-    i=i+1;
-    fitmodel=FitSynapseSize(fitsim,testsim,'Display',false,varargin{:});
-    err_data.num_data(i) = fitsim.NumT + testsim.NumT;
-    if truemodel.SameSizes(fitmodel)
-        [~,kl]=truemodel.KLdivs(fitmodel);
-        [~,L2]=truemodel.LnNorm(2,fitmodel);
-        err_data.KLdiv(i)=mean(kl)/truemodel.NumStates;
-        err_data.L2err(i)=sum(L2);
-    end%if SameSizes
+for i=1:ceil(log2(MaxLen/StartLen))
+    StartLen=StartLen*2;
+    if Display
+        disp(StartLen);
+    end
+    klv=NaN(1,NumReps);
+    L2v=klv;
+    for j=1:NumReps
+        fitsim=truemodel.Simulate(fp,rand(2,DataPerChunk,StartLen));
+        testsim=truemodel.Simulate(fp,rand(2,TestDataPerChunk,StartLen));
+        %
+        err_data.num_data(i) = fitsim.NumT + testsim.NumT;
+        %
+        fitmodel=FitSynapseSize(fitsim,testsim,'Display',false,varargin{:});
+        if truemodel.SameSizes(fitmodel)
+            [~,kl]=truemodel.KLdivs(fitmodel);
+            [~,L2]=truemodel.LnNorm(2,fitmodel);
+            klv(j)=mean(kl)/truemodel.NumStates;
+            L2v(j)=sum(L2);
+        end%if SameSizes
+    end%for j
+    klv(isnan(klv))=[];
+    L2v(isnan(L2v))=[];
+    if length(klv)>=SuccessFrac*NumReps
+        err_data.KLdiv_mean(i)=mean(klv);
+        err_data.KLdiv_err(i)=std(klv)/sqrt(length(klv));
+        err_data.L2_mean(i)=mean(L2v);
+        err_data.L2_err(i)=std(L2v)/sqrt(length(L2v));
+    end%if length(klv
 end
 
-
-    function DoubleData
-        extrasim=truemodel.Simulate(fp,rand(2,DataPerChunk,max(1,length(fitsim))));
-        fitsim=[fitsim extrasim];
-        %
-        extrasim=truemodel.Simulate(fp,rand(2,TestDataPerChunk,max(1,length(testsim))));
-        testsim=[testsim extrasim];
-        %
-        if Display
-            disp(length(fitsim));
-        end%if Display
-    end%function DoubleData
 
 end
 
