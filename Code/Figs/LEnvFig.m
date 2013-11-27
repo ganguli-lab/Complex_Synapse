301867cb4b08ec3aeda7116ead24177d1e13bc7f
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2013-11-27 13:13:00 -0800

    running average envelope fig

--- Code/Figs/LEnvFig.m
+++ Code/Figs/LEnvFig.m
@@ -7,21 +7,27 @@ embiggen;
 xlim([1/max(srange) 1/min(srange)]);
 set(gca,'XTick',[1e-3,1e-2,1e-1,1,1e1,1e2,1e3]);
 hold on;
-for n=nrange
+%
+[Wp,Wm,w]=SerialBuilder(nrange(end),1);
+%     ch=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'r:','LineWidth',1.5);
+uh=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b-','LineWidth',2);
+%
+for n=nrange(1:end-1)
 %     [Wp,Wm,w]=DiffJump(n);
 %     sh=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b--','LineWidth',1.5);
     [Wp,Wm,w]=SerialBuilder(n,1);
 %     ch=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'r:','LineWidth',1.5);
-    ch=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b--','LineWidth',1.5);
+    ch=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b--','LineWidth',2);
 end
-epsvals=0.1.^(1:0.5:2);
+epsvals=0.1.^(0.5:0.5:2);
 for eps=epsvals
     q=ones(1,11);
     q(1)=eps;
     [Wp,Wm,w]=MakeSMS(q);
-    sh=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b:','LineWidth',1.5);
+    sh=loglog(1./srange,10*SNRrunAve(1./srange,Wp,Wm,0.5,w),'b:','LineWidth',2);
 end
 % legend([eh ch sh],{'Numeric Envelope','Serial','Shortcut'},'Location','Best');
-legend([eh ch sh],{'Numeric Envelope','Shorter chains','Sticky end states'},'Location','Best');
-clear nrange eh sh ch Wp Wm w;
-clear epsvals eps q;
+legend([eh uh ch sh],{'Numeric Envelope','Uniform serial','Shorter chains','Sticky end states'},'Location','Best');
+clear nrange eh sh ch Wp Wm w n;
+clear epsvals eps q uh;
+xlim([1e-2 1e3])
