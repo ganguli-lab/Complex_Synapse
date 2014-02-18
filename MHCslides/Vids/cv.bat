17fe26d1794276e13cee2966aab56aab441a664b
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2014-02-18 13:52:11 -0800

    improved plasticity video

--- MHCslides/Vids/cv.bat
+++ MHCslides/Vids/cv.bat
@@ -1,5 +1,5 @@
 @echo off
 
 for /F "delims=" %%f in ('dir /s /b *.svg') do (
-"C:\Program Files (x86)\Inkscape\inkscape" -f %%~nf.svg -d 300 -e %%~nf.png
+"C:\Program Files (x86)\Inkscape\inkscape" -f %%~nf.svg -w 1600 -h 736 -e %%~nf.png
 )
