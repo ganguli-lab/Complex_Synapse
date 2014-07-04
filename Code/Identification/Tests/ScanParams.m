5f1dc2e7fe50eff2ad33d01723a2edaad30becf6
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2014-07-03 17:48:25 -0700

    notation changes

--- Code/Identification/Tests/ScanParams.m
+++ Code/Identification/Tests/ScanParams.m
@@ -5,7 +5,7 @@ tolxvals=[1e-2 5e-3 1e-3 5e-4 1e-4 5e-5 1e-5];
 scan_results(length(tolfunvals),length(tolxvals))=...
     struct('num_events',[],'prob_st',[],'KL',[],'Ln',[]);
 
-options=SynapseOptimset('MaxStates',modelobj.NumStates/2);
+options=SynapseOptimset('MaxStates',modelobj.NumStates/2,'ModelDiff','Ln');
 
 for i=1:length(tolfunvals)
     for j=1:length(tolxvals)
