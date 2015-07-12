0fb15b4662b6172da44f93940867d9598842f25e
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2015-07-12 13:47:42 -0700

    ylim position

--- Code/Optimum/Envelopes/DoubleLenvPlot.m
+++ Code/Optimum/Envelopes/DoubleLenvPlot.m
@@ -8,10 +8,10 @@ function DoubleLenvPlot( env,AenvSingle,ind,varargin )
 loglog(env.tau,AenvSingle./env.tau,'g',env.tau,env.Aenv,'r',1/env.sc,env.sc*env.Ac,'rd',varargin{:});
 
 hold on;
-yl=ylim;
 
 if exist('ind','var') && ~isempty(ind)
     loglog(env.tau,env.mats(ind).modelobj.SNRrunAve(env.tau),'b',varargin{:});
+    yl=ylim;
     line([1 1]/env.mats(ind).s,yl,'Color','k',varargin{:});
 end
 
