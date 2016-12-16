e85cd444d522826c8bfa9c8d2b3f1b5f2d66fc0f
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2016-12-16 14:13:14 -0800

    longterm training figs for wrong models

--- Code/VOR/Plots/LongTermPre.m
+++ Code/VOR/Plots/LongTermPre.m
@@ -24,6 +24,30 @@ df = {0.5, 0.3, 0.9};
 tpre = 500;
 train=800;
 %%
+builder_h = @SerialBuilder;
+n = 2;
+pwt = [0.1 0.1];
+pko = 0.2;
+df = {0.5, 0.4, 0.6};
+tpre = 5;
+train=40;
+%%
+builder_h = @MultistateBuilder;
+n = 10;
+pwt = [0.3 0.3];
+pko = 0.4;
+df = {0.5, 0.2, 0.8};
+tpre = 5;
+train=20;
+%%
+builder_h = @PooledBuilder;
+n = 10;
+pwt = [0.008 0.0006 0.6];
+pko = [0.001 1];
+df = {0.5, 0.1, 0.9};
+tpre = 5;
+train=800;
+%%
 vexpt=VORbuilderKO(builder_h, n, pwt(1), pwt(2), pko, df{:},train,tpre, false);
 vexpt.PlotLearnS('all',false,'LineWidth',2);
 xlim([0 train]);
@@ -36,3 +60,12 @@ print('cascade_longterm.eps','-depsc');
 %%
 title('Non-uniform multistate model','FontSize',vexpt.FontSize);
 print('nonuni_longterm.eps','-depsc');
+%%
+title('Two-state model','FontSize',vexpt.FontSize);
+print('binary_longterm.eps','-depsc');
+%%
+title('Multistate model','FontSize',vexpt.FontSize);
+print('multistate_longterm.eps','-depsc');
+%%
+title('Pooled resource model','FontSize',vexpt.FontSize);
+print('pooled_longterm.eps','-depsc');
