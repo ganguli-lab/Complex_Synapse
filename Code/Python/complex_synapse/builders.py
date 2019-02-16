fixes broadcasting with array s

Code/Python/complex_synapse/builders.py
@@ -129,8 +129,8 @@ def rand_trans(nst: int, npl: int = 1, sparsity: float = 1.) -> la.lnarray:
     mat : la.lnarray
         transition matrix
     """
-    mat = np.random.rand(npl, nst, nst)
-    ind = np.random.rand(npl, nst, nst)
+    mat = la.random.rand(npl, nst, nst)
+    ind = la.random.rand(npl, nst, nst)
     mat[ind > sparsity] = 0.
     stochastify_c(mat)
     return mat
@@ -187,7 +187,7 @@ def build_generic(func, nst: int, npl: int = 2,
         signal : la.lnarray
             desired signal contribution from each plasticity type
     """
-    plast = func(nst, npl)
+    plast = la.asarray(func(nst, npl))
     if binary:
         weight = binary_weights(nst)
     else:

Code/Python/complex_synapse/synapse_base.py
@@ -116,7 +116,7 @@ class SynapseBase(la.gufuncs.LNArrayOperatorsMixin):
         """Complete frac vector
         """
         if len(self.frac) == len(self.plast) - 1:
-            self.frac = np.concatenate((self.frac, [1. - self.frac.sum()]))
+            self.frac = la.concatenate((self.frac, [1. - self.frac.sum()]))
 
     def valid_shapes(self) -> bool:
         """Do attributes (plast, weight, frac) have correct shapes?"""

Code/Python/complex_synapse/synapse_memory_model.py
@@ -175,12 +175,12 @@ class SynapseMemoryModel(SynapseBase):
         onev = la.ones_like(self.weight)
         if rowv is None:
             rowv = onev
-        fundi = onev.c * rowv - self.markov()
-        if rate is not None:
+        if rate is None:
+            s_arr = 0
+        else:
             # convert to lnarray, add singletons to broadcast with matrix
-            s_arr = la.asarray(rate).s
-            fundi += s_arr * np.eye(self.nstates)
-        return fundi
+            s_arr = la.asarray(rate).s * np.eye(self.nstates)
+        return onev.c * rowv - self.markov() + s_arr
 
     def peq(self) -> la.lnarray:
         """Steady state distribution.
@@ -221,7 +221,7 @@ class SynapseMemoryModel(SynapseBase):
         .. math:: Z(s) = (s I + e \pi - W^f)^{-1} \simeq \int e^{t(W^f-sI)} dt,
 
         i.e. :math:`Z^{-1}` with :math:`s` added to the diagonal.
-        Effectively the matrix inverse of the genarator's Laplace transform.
+        Effectively the matrix inverse of the propagator's Laplace transform.
 
         See Also
         --------
@@ -261,8 +261,8 @@ class SynapseMemoryModel(SynapseBase):
         .. math:: \\eta_i = \\sum_j T_{ij}(s) \\pi_j w_j.
         .. math:: \\delta \\eta_{ij}(s) = \\eta_i(s) - \\eta_j(s).
         """
-        eta = - self.zinv(rate).inv @ self.weight
-        return eta.c - eta
+        eta = - self.zinv(rate).inv @ self.weight.c
+        return eta - eta.t
 
     # -------------------------------------------------------------------------
     # %%* Memory curves
@@ -307,7 +307,7 @@ class SynapseMemoryModel(SynapseBase):
         rate : float, array, optional
             Parameter of Laplace transform, ``s``. Default: 0.
         """
-        return np.sum(self.peq() @ (self.enc() * self.deta(rate)))
+        return np.sum(self.peq() @ (self.enc() * self.deta(rate)), axis=-1)
 
     def snr_exp_ave(self, tau: ArrayLike) -> la.lnarray:
         """Exponential running average of SNR memory curve.